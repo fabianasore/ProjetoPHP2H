b91d1a73f7b3850172aab1ecf1c6957baf3b5b92
fabianasore <fabianasore@gmail.com>
2021-08-19 22:19:58 -0300

    atualizado

---
+++ segundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	#Comentário
+	#Data: 19/08/2021
+ ?>
+<head>
+	<title> PHP - Testando Comentário</title>
+</head>
+<body>
+	<?php 
+		/*
+		Comentário de linha
+		
+		*/
+		echo "Segundo Exemplo de Comentário";
+		$data = "19 de agosto de 2021";
+	 ?>
+	<p> Este paragráfo está sendo interpretado pelo HTML</p>
+	Arquivo criado em php <?php echo $data; ?>
+
+
+</body>
+</html>
